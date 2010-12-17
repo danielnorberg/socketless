6276fea9dda12cc9e46087de2d6954e0bc78060e
Daniel Norberg <daniel.norberg@gmail.com>
2010-12-17 23:35:19 +0100

    fixed packaging added syncless dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 
 modules = ['broadcast', 'channel', 'messenger', 'ropebuffer', 'streamserver']
-extensions = [Extension('socketless.%s' % module, ['socketless/%s.c' % module]) for module in modules]
+
+extensions = [Extension(
+	name ='socketless.%s' % module,
+	sources = ['socketless/%s.c' % module],
+	extra_compile_args = ['-w'],
+) for module in modules]
 
 setup(
 	name='socketless',
 	version='0.1',
 	packages=['socketless'],
     ext_modules = extensions,
+	install_requires = ['syncless>=0.17'],
 )
