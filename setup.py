20d556002d9cb722f7c25ed13f603bfa36ad04bd
Daniel Norberg <daniel.norberg@gmail.com>
2010-12-18 00:27:41 +0100

    more packaging

--- setup.py
+++ setup.py
@@ -14,5 +14,35 @@ setup(
 	version='0.1',
 	packages=['socketless'],
     ext_modules = extensions,
-	install_requires = ['syncless>=0.17'],
+    install_requires = ['syncless>=0.17'],
+    zip_safe=True,
+
+	author = "Daniel Norberg",
+    author_email = "daniel.norberg@gmail.com",
+    url = "https://github.com/danielnorberg/socketless/",
+    description = "Socketless: An asynchronous high performance TCP messaging library.",
+	long_description = 	"""\
+		Socketless is a an asynchronous TCP messaging library for
+		implementing communication using messages and broadcast req/rep
+		instead of raw sockets. Socketless relies on the library syncless
+		to provide high-performance non-blocking sockets through utilization
+		of epoll/kevent/kqueue. Socketless is implemented in Cython for high througput.
+	""",
+	license="Apache License, Version 2.0",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: No Input/Output (Daemon)",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
